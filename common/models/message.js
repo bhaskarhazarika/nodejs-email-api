041252ba59d685bf8caeabb905c52500a764300a
Bhaskar Hazarika <bhaskarjyoti86@gmail.com>
2015-12-07 23:30:30 -0500

    added welcome template

--- common/models/message.js
+++ common/models/message.js
@@ -18,7 +18,7 @@ Message.observe('after save', function(ctx, next) {
 	Message.app.models.Email.send({
 	      to: "bhaskarjyoti86@gmail.com",
 	      from: "node2test@gmail.com",
-	      subject: 'Password reset',
+	      subject: 'Order Details',
 	      html: output
 	    }, function(err) {
 	      if (err) return console.log('> error sending password reset email',err);
